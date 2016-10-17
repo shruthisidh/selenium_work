5c9d82f85a30afb46a33b768996c9729449456a5
shruthims <shruthi.sidh@gmail.com>
2016-10-17 14:10:41 -0700

    CrossBrowsingTesting

---
+++ webtests/src/main/java/com/CrossBrowsing/IndividualCrossBrowsingInXMl.java
@@ -0,0 +1,33 @@
+package com.CrossBrowsing;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class IndividualCrossBrowsingInXMl {
+	WebDriver driver;
+	@Test
+	@Parameters("browser")
+	public void getbrowsers(String browsername){
+		if(browsername.equalsIgnoreCase("firefox")){
+			driver = new FirefoxDriver();
+		}
+//		else if(browsername.equalsIgnoreCase("chrom")){
+//			System.setProperty("WebDriver.chrome.driver", "chromeDriver.exe path");
+//			driver.new chromeDriver();
+//		}else if(browsername.equalsIgnoreCase("ie")){
+//			System.setProperty("WebDriver.ie.Driver","ie.exe path");
+//			driver.new ieDriver();
+//		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	    driver.get("http://www.demo.guru99.com/V4/index.php");
+		System.out.println( driver.getTitle());
+			
+	}
+
+}
